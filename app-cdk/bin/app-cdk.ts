2a193989d5e47f0431461d939b69c699fbd1e7d7
hide1080 <hide1080@gmail.com>
2025-04-22 23:56:56 +0900

    Added Docker build and push

--- app-cdk/bin/app-cdk.ts
+++ app-cdk/bin/app-cdk.ts
@@ -2,8 +2,12 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { AppCdkStack } from '../lib/app-cdk-stack';
 import { PipelineCdkStack } from '../lib/pipeline-cdk-stack';
+import { EcrCdkStack } from '../lib/ecr-cdk-stack';
 
 
 const app = new cdk.App();
 const appCdkStack = new AppCdkStack(app, 'app-stack', {});
-const pipelineCdkStack = new PipelineCdkStack(app, 'pipeline-stack', {});
+const ecrCdkStack = new EcrCdkStack(app, 'ecr-stack', {});
+const pipelineCdkStack = new PipelineCdkStack(app, 'pipeline-stack', {
+  ecrRepository: ecrCdkStack.repository,
+});
